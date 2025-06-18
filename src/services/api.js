9ea1345c2217ed4bc49d456cdc41cd81b9736087
Ana Carolina Kobayashi <ana.kobayashi@ncibr.local>
2025-06-18 16:09:17 -0400

    Atualiza Home.js com melhorias de layout/conteúdo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://frontend-salas.onrender.com'  // sua URL pública do Render
+  baseURL: 'https://agendamento-salas.onrender.com'  // sua URL pública do Render
 });
 
 export default api;
