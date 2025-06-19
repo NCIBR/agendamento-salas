2481bfe682c0d65acfa18a38a39abef8dba68236
Ana Carolina Kobayashi <ana.kobayashi@ncibr.local>
2025-06-19 11:34:58 -0400

    ATUALIZA DE NOVO

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#eee', padding: '10px' }}>
+    <nav style={{
+      background: '#eee',
+      padding: '10px',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      zIndex: 1000
+    }}>
       <Link to="/" style={{ marginRight: '10px' }}>Início</Link>
       <Link to="/agendar" style={{ marginRight: '10px' }}>Agendar Sala</Link>
       <Link to="/agendamentos">Ver Agendamentos</Link>
